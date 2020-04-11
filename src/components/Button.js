e3bdc378992c569df4b04249f83f5dd8ac01b84e
isopoju <50376880+isopoju@users.noreply.github.com>
2020-04-11 23:26:35 +0300

    Button.js lisatty onClick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Button = ({ buttonText, priority, type, cyDataAttribute }) => (
+const Button = ({ buttonText, priority, type, cyDataAttribute, onClick }) => (
   <div className="button__container">
     <button
       className={`button button-${priority} button-${type}`}
       data-cy={cyDataAttribute}
       type={type}
+      onClick={onClick}
     >
       {buttonText}
     </button>
