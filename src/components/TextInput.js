b098ab00e2d8fc0e3bde7df1428cc68b7f09bd37
isopoju <50376880+isopoju@users.noreply.github.com>
2020-04-11 21:18:52 +0300

    TextInput.js spaces 2

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const TextInput = ({ label, inputValue }) => {
 
-    // eslint-disable-next-line
-    const removeReset = ({ reset: _, ...clone }) => clone
+  // eslint-disable-next-line
+  const removeReset = ({ reset: _, ...clone }) => clone
 
-    return (
-        <div className='form__field form__field--text'>
-            <label>{label}</label>
-            <input
-                data-cy="tip-url"
-                id="tip_url"
-                {...removeReset(inputValue)}
-            />
-        </div>
-    )
+  return (
+    <div className='form__field form__field--text'>
+      <label>{label}</label>
+      <input
+        data-cy="tip-url"
+        id="tip_url"
+        {...removeReset(inputValue)}
+      />
+    </div>
+  )
 }
 
 export default TextInput
