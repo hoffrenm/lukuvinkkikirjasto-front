0448760a74ba62dcb5c9cf71916fc712b840bc48
isopoju <50376880+isopoju@users.noreply.github.com>
2020-04-11 21:16:42 +0300

    Button.js spaces 2

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Button = ({ buttonText, priority, type, cyDataAttribute }) => (
-    <div className="button__container">
-        <button
-            className={`button button-${priority} button-${type}`}
-            data-cy={cyDataAttribute}
-            type={type}
-        >
-            {buttonText}
-        </button>
-    </div>
+  <div className="button__container">
+    <button
+      className={`button button-${priority} button-${type}`}
+      data-cy={cyDataAttribute}
+      type={type}
+    >
+      {buttonText}
+    </button>
+  </div>
 )
 
 export default Button
